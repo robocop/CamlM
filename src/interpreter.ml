1b8356e4634955c8e36777b2081d43b656942127
Charlie P <paucardcharlie@gmail.com>
2012-07-18 19:31:05 +0200

    Added minimal mode for interpreter

--- src/interpreter.ml
+++ src/interpreter.ml
@@ -23,11 +23,18 @@ let scan () =
         end
   in scan' 0 ""
 
+let minimal = ref false
+let usage = "usage: " ^ Sys.argv.(0) ^ " [--minimal]"
+let speclist = [
+  ("--minimal", Arg.Unit (fun () -> minimal := true), "run with minimal onscreen content")
+]
+
 let _ =
+  let input () = if not !minimal then "# " else "" in
   let rec loop fn_env type_env =
     try 
       begin
-      let lexbuf = Lexing.from_string (scan (print_string "# ")) in
+      let lexbuf = Lexing.from_string (scan (print_string (input ()))) in
         match parse Parser.eval lexbuf with
           | INothing -> print_endline "Done"
           | ICommand com -> (match com with
@@ -37,10 +44,14 @@ let _ =
           | IValue res -> 
               let (type_env', t) = type_expr type_env res in
               let (fn_env', value) = eval (fn_env, builtin_ops) res
-              in Printf.printf ":- %s = " (print_type t);	    
+              in if not !minimal then Printf.printf ":- %s = " (print_type t) else ();
                  print_endline (show value); 
                  loop fn_env' type_env'
       end
     with exn -> handle_error exn; loop fn_env type_env
-  in try loop builtin_fns builtin_types
-  with exn -> handle_error exn; loop builtin_fns builtin_types
+  in
+   Arg.parse speclist (fun x -> raise (Arg.Bad ("Bad argument : " ^ x)))
+    usage; 
+    
+   try loop builtin_fns builtin_types
+    with exn -> handle_error exn; loop builtin_fns builtin_types
