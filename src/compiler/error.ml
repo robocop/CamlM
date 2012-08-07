b6e9a42ead234d97a32688c23fa1dc985391ddda
Charlie P <paucardcharlie@gmail.com>
2012-08-07 18:16:03 +0200

    Commented Error module

--- src/compiler/error.ml
+++ src/compiler/error.ml
@@ -1,13 +1,33 @@
+(** Exception definitions and pretty print function.
+   
+    For now, most errors cause immediate termination of any eval/typing function.
+    It is the responsability of the front end (CLI, REPL) to handle those errors.
+  *)
 open Lexing
 
+(** Matching failed on current pattern. See the source code for {!Eval} to see
+    how this is used.*)
 exception MatchingFailure
+
+(** Generic error. *)
 exception Error of string
+
+(** Parse error at specific position. *)
 exception ParseError of Lexing.position * string
+
+(** Type system caught in a loop. *)
 exception Loop of string * string
+
+(** Type conflict during unification. *)
 exception Conflit of string * string
+
+(** Multiple, equally valid names returned from an environment lookup. *)
 exception MultiDef of string * string list
+
+(** Name was not found in environment lookup. *)
 exception Undef of string
 
+(** Pretty print function. *)
 let handle_error = function
   | Error s -> print_endline ("Error : " ^ s) 
   | Undef f -> print_endline ("Error : " ^ f ^ " is not defined")
